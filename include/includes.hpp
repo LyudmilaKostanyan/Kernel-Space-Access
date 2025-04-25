29379e4ec69941274f5915e58093baa61763447e
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-04-25 21:02:28 +0400

    Reorganize includes in includes.hpp for improved clarity and structure

--- include/includes.hpp
+++ include/includes.hpp
@@ -4,9 +4,20 @@
 #include <utility>
 #include <cstdint>
 #include <cstdlib>
-#include <setjmp.h>
-#include <signal.h>
+#include <unistd.h>
+#include <csignal>
+#include <csetjmp>
+#include <pthread.h>
 
+#if defined(__APPLE__)
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
+#include <mach/vm_region.h>
+#endif
+
+#if defined(_WIN32)
+#include <windows.h>
+#endif
 
 std::pair<void*, void*> get_stack_bounds();
 
